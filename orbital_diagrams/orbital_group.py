f58e86119a50d00c6a239b5402505a5c5a9487bc
Jonathon Vandezande <jevandezande@gmail.com>
2022-02-08 11:35:12 -0500

    Simplifies OrbitalGroup testing.

--- orbital_diagrams/orbital_group.py
+++ orbital_diagrams/orbital_group.py
@@ -8,8 +8,11 @@ from ._base_orbital import BaseOrbital
 class OrbitalGroup(BaseOrbital):
     orbs: list[BaseOrbital]
 
+    def __len__(self) -> int:
+        return len(self.orbs)
+
     def __iter__(self) -> Iterator[BaseOrbital]:
         yield from self.orbs
 
     def __str__(self) -> str:
-        return f"<OrbitalGroup {self.orbs}>"
+        return f"<{type(self).__name__} {self.orbs}>"
--- tests/test_OrbitalGroup.py
+++ tests/test_OrbitalGroup.py
@@ -1,24 +1,26 @@
-from pytest import fixture
-
 from orbital_diagrams import OrbitalGroup
 from orbital_diagrams._base_orbital import BaseOrbital
 
 
-@fixture
-def orbs_4():
-    return [BaseOrbital() for _ in range(4)]
+def gen_orb_group(size):
+    return OrbitalGroup([BaseOrbital() for _ in range(size)])
 
 
-def test_init(orbs_4):
-    og = OrbitalGroup(orbs_4)
-    assert list(orbs_4) == og.orbs
+def test_init():
+    orbs = [BaseOrbital() for _ in range(4)]
+    og = OrbitalGroup(orbs)
+    assert og.orbs == orbs
 
 
-def test_iter(orbs_4):
-    og = OrbitalGroup(orbs_4)
+def test_len():
+    assert len(gen_orb_group(4)) == 4
+
+
+def test_iter():
+    og = gen_orb_group(4)
     assert list(og) == og.orbs
 
 
-def test_str(orbs_4):
-    og = OrbitalGroup(orbs_4)
+def test_str():
+    og = gen_orb_group(4)
     assert str(og) == f"<OrbitalGroup {list(og)}>"
