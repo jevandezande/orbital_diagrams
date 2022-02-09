9d4ba47137230d7840e39a6c5e55f2eb7fceeaee
Jonathon Vandezande <jevandezande@gmail.com>
2022-02-09 11:51:12 -0500

    Simplifies test_OrbitalGroup code.

--- tests/test_OrbitalGroup.py
+++ tests/test_OrbitalGroup.py
@@ -1,3 +1,5 @@
+from pytest import fixture
+
 from orbital_diagrams import OrbitalGroup
 from orbital_diagrams._base_orbital import BaseOrbital
 
@@ -6,21 +8,24 @@ def gen_orb_group(size):
     return OrbitalGroup([BaseOrbital() for _ in range(size)])
 
 
+@fixture
+def og_4():
+    return gen_orb_group(4)
+
+
 def test_init():
     orbs = [BaseOrbital() for _ in range(4)]
     og = OrbitalGroup(orbs)
     assert og.orbs == orbs
 
 
-def test_len():
-    assert len(gen_orb_group(4)) == 4
+def test_len(og_4):
+    assert len(og_4) == 4
 
 
-def test_iter():
-    og = gen_orb_group(4)
-    assert list(og) == og.orbs
+def test_iter(og_4):
+    assert list(og_4) == og_4.orbs
 
 
-def test_str():
-    og = gen_orb_group(4)
-    assert str(og) == f"<OrbitalGroup {list(og)}>"
+def test_str(og_4):
+    assert str(og_4) == f"<OrbitalGroup {list(og_4)}>"
